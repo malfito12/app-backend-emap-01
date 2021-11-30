02e66274e440feced002dd0c0fc3c34eededf3a6
Alex <alextufielamigo@gmail.com>
2021-11-29 22:41:13 -0400

    cambio de direccion heroku (planillas restrospectiva y aportespatronales)

--- src/database.js
+++ src/database.js
@@ -5,19 +5,19 @@ const URI = process.env.MONGODB_URI
     : 'mongodb://localhost/databasetest';
 
 const databasename = 'crud'
-mongoose.connect("mongodb://127.0.0.1:27017/" + databasename, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false
-})
-
-// mongoose.connect("mongodb+srv://malfito12:vivabraun123@dbcluster.yhudn.mongodb.net/EMAP?retryWrites=true&w=majority",{
+// mongoose.connect("mongodb://127.0.0.1:27017/" + databasename, {
 //     useNewUrlParser: true,
 //     useCreateIndex: true,
 //     useUnifiedTopology: true,
-//     useFindAndModify:false
+//     useFindAndModify: false
 // })
+
+mongoose.connect("mongodb+srv://malfito12:vivabraun123@dbcluster.yhudn.mongodb.net/EMAP?retryWrites=true&w=majority",{
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify:false
+})
 
 const connection=mongoose.connection
 
